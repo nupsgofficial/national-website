822066e77c3e9243b80f11a0533d8cc48cc01423
Yaw Karikari Wiafe <kaykayyaw@gmail.com>
2023-11-05 16:55:38 +0000

    adjust height of displays

--- src/components/Displays.jsx
+++ src/components/Displays.jsx
@@ -2,7 +2,7 @@ const Displays = ({ title }) => {
   return (
     <div className="bg-[#28203E] text-color-1 font-bold  border  rounded-lg font-poppins mb-8">
       <h3 className="bg-[#4A41CF] text-center border rounded-t-lg">{title}</h3>
-      <p className="px-4 py-2 h-36"></p>
+      <p className="px-4 py-2 h-44"></p>
     </div>
   );
 };
